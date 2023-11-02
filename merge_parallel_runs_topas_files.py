0d50034d55eb7d761fedab15f2943630beb07ce8
HMiras <hector.miras@gmail.com>
2023-11-02 10:55:40 +0100

    Read file_patterns from simconfig.json if it exists.

--- merge_parallel_runs_topas_files.py
+++ merge_parallel_runs_topas_files.py
@@ -5,17 +5,25 @@ import json
 import os
 import sys
 
-sim_path = sys.argv[1]
-#sim_path = "/home/hector/mytopassimulations/MGHsimulations/tests/nodes_output"
+#sim_path = sys.argv[1]
+sim_path = "/home/hector/mytopassimulations/MGHsimulations/tests/nodes_output"
 output_dir = os.path.join(sim_path, "results")
 
 file_patterns = ["DoseToCell*", "DoseToNucleus*electrons.csv", "DoseToNucleus*gammas.csv", "nucleus_PHSP*"]
+
+# If simconfig.json file exists in the directory, get the OUTPUT_FILE_PATTERNS from the file
+if os.path.isfile('simconfig.json'):
+    # Open the file and read the JSON content
+    with open('simconfig.json', 'r') as file:
+        data = json.load(file)
+        file_patterns = data.get('OUTPUT_FILE_PATTERNS', [])  # Provide default empty list if key doesn't exist
+        print(f"File patterns for reduce: {file_patterns}")
 
 for file_pattern in file_patterns:
     output_file_paths = get_outputfile_paths(sim_path, file_pattern)
     if len(output_file_paths) > 0:
         if output_file_paths[0].suffix == ".csv":
-            merge_combine_csv(output_file_paths, output_dir)
+            merge_combine_csv(output_file_paths=output_file_paths, output_path=output_dir, append=True)
         if (output_file_paths[0].suffix == ".phsp") or (output_file_paths[0].suffix == ".header"):
             phsp_files = [f for f in output_file_paths if f.suffix == ".phsp"]
             merge_phsp_files(phsp_files, output_dir)
