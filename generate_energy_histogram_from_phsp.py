5572ea05e7b8ab8b4ae131283dbf3a8fcf119fa8
HectorMiras <hector.miras@gmail.com>
2024-11-08 10:56:45 +0100

    jupyter file for results

--- generate_energy_histogram_from_phsp.py
+++ generate_energy_histogram_from_phsp.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from phsp_manager import generate_weighted_energy_histogram
 
-folder = '/home/hector/mytopassimulations/MGHsimulations/TOPAS_CellsNPs/work/smallcellresults/topas-azure-med1-cells1-phspnp/nodes_output/results'
-filename = 'PhaseSpace_NP' # without extension
+folder = '/home/hector/mytopassimulations/MGHsimulations/TOPAS_CellsNPs/work/smallcellresults/topas-azure-med0-cell0/nodes_output/results'
+filename = 'nucleus_PHSP_medium0p0mgml_cell0p0mgml_AuNP15_electrons' # without extension
 min_energy = 0
 max_energy = 0.035
-bins = 50
-particle = 'gammas' # 'electrons', 'gammas'
+bins = 100
+particle = 'electrons' # 'electrons', 'gammas'
 output_txt = True
 show = True
 save_path = True
