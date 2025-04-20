92492d47cbee56322b688e5825180820d35695c5
HectorMiras <hector.miras@gmail.com>
2025-04-20 15:08:37 +0200

    commit de seguridad 20 abr

--- analyze_NP_phsp.py
+++ analyze_NP_phsp.py
@@ -1,17 +1,17 @@
 from phsp_manager import generate_weighted_energy_histogram, np_stats
 
-folder = '/home/hector/mytopassimulations/MGHsimulations/TOPAS_CellsNPs/work/smallcellresults/topas-azure-med0p05-cells0p05/results'
+folder = '/home/radiofisica/hector/mytopassimulations/TOPAS_CellsNPs/work/test-med1-smallcell1/results'
 filename = 'PhaseSpace_NP' # without extension
 
 
 min_energy = 0
 max_energy = 0.025
-bins = 100
+bins = 1000
 particle = 'electrons' # 'electrons', 'gammas'
 output_txt = True
 show = False
 save_path = True
-#generate_weighted_energy_histogram(folder, filename, bins, min_energy, max_energy, particle, show, save_path, output_txt)
+generate_weighted_energy_histogram(folder, filename, bins, min_energy, max_energy, particle, show, save_path, output_txt)
 
 min_energy = 0
 max_energy = 0.020
